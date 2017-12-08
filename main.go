877cefdeb72508da033ca22adc0b4f1cd47d8acc
Victor Webb <vwebb@vwebb-macbookpro.roam.corp.google.com>
2017-12-07 17:48:26 -0800

    added index.html and modified main.go to serve index.html

--- main.go
+++ main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
+	"log"
 	"net/http"
-
-	"google.golang.org/appengine"
 )
 
-const homepage = '<!doctype html>
-<html>
-<head>
-	<style>
-		body {
-			background-color: blue;
-		}
-	</style>
-</head>
-<body>
-	<center><h1><font color=white>
-		{{ . }}
-	</font></h1></center>
-</body>
-</html>'
-
-func homepageHandler(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.Parse(homepage)
-	t.Execute(w, "Hello GCP!")
-	//fmt.Fprint(w, homepage)
+func hello(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, "Hello World!")
 }
 
 func main() {
-	http.HandleFunc("/", homepageHandler)
-	appengine.Main()
+	http.HandleFunc("/", hello)
+	fmt.Println("Server running...")
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
