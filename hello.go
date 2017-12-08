5cb1d2e24c43a7c0be77955d7d950f6676502851
Victor Webb <vwebb@vwebb-macbookpro.roam.corp.google.com>
2017-12-07 19:49:16 -0800

    fix

--- hello.go
+++ hello.go
@@ -5,30 +5,31 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 )
+
+func main() {
+	http.HandleFunc("/", hello)
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	// appengine.Main()
+	fmt.Println("Server running...")
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+var tpl = template.Must(template.ParseGlob("templates/*.html"))
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	ctx := appengine.NewContext(r)
-	log.Infof(ctx, "got appengine context")
-	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, "Hello World!")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if err := tpl.ExecuteTemplate(w, "index.html", "Hello World!"); err != nil {
+		log.Fatal(err)
+	}
+	// t, _ := template.ParseFiles("index.html")
+	// t.Execute(w, "Hello World!")
 }
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "ok")
-}
-
-func main() {
-	http.HandleFunc("/", hello)
-	http.HandleFunc("/_ah/health", healthCheckHandler)
-	appengine.Main()
-	// fmt.Println("Server running...")
-	// log.Fatal(http.ListenAndServe(":80", nil))
 }
